eac496268fa6c2e876b90d915a3fbac5b6b5a496
Mauricio7274 <99046250+Mauricio7274@users.noreply.github.com>
2022-04-28 21:54:01 -0500

    Add files via upload

---
+++ WebApplication1/WebApplication1/Datos/Modelo/Estudiante.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Datos.Modelo
+{
+    public class Estudiante
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Universidad { get; set; }
+        public string Carrera { get; set; }
+        public int Semestre { get; set; }
+
+    }
+}
